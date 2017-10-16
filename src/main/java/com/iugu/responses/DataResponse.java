9cca7e0d82d88f546bf9863a1435c4bd59ae7cdb
Daniel Felgar <daniel@lojafacil.com.br>
2017-10-16 20:50:46 -0200

    change to gson

---
+++ src/main/java/com/iugu/responses/DataResponse.java
@@ -0,0 +1,58 @@
+package com.iugu.responses;
+
+public class DataResponse {
+    private String brand;
+    private String holderName;
+    private String displayNumber;
+    private String bin;
+    private String month;
+    private String year;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getHolderName() {
+        return holderName;
+    }
+
+    public void setHolderName(String holderName) {
+        this.holderName = holderName;
+    }
+
+    public String getDisplayNumber() {
+        return displayNumber;
+    }
+
+    public void setDisplayNumber(String displayNumber) {
+        this.displayNumber = displayNumber;
+    }
+
+    public String getBin() {
+        return bin;
+    }
+
+    public void setBin(String bin) {
+        this.bin = bin;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
