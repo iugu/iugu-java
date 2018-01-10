e9b8ece7c7ae2c9651afb6cf3b1c67074d458c4d
DanielDPN <danielpereiradonascimento@gmail.com>
2018-01-10 10:50:48 -0300

    Lista de SubscriptionReponse inserida na classe SubscriptionsResponse.

--- src/main/java/com/iugu/responses/SubscriptionsResponse.java
+++ src/main/java/com/iugu/responses/SubscriptionsResponse.java
@@ -1,11 +1,13 @@
 package com.iugu.responses;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SubscriptionsResponse {
 
     private Integer totalItems;
+    private List<SubscriptionResponse> items;
 
     public Integer getTotalItems() {
         return totalItems;
@@ -15,8 +17,16 @@ public class SubscriptionsResponse {
         this.totalItems = totalItems;
     }
 
+    public List<SubscriptionResponse> getItems() {
+        return items;
+    }
+
+    public void setItems(List<SubscriptionResponse> items) {
+        this.items = items;
+    }
+
     @Override
     public String toString() {
-        return "SubscriptionsResponse{" + "totalItems=" + totalItems + '}';
+        return "SubscriptionsResponse{" + "totalItems=" + totalItems + ", items=" + items + '}';
     }
 }
