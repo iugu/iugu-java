dd113595ef8a9897fcc26131c59329e16c966cef
Rafael <rafael@tartigrado.com>
2018-03-24 12:43:38 -0300

    Ajuste no model de transferência

--- src/main/java/com/iugu/model/Transfer.java
+++ src/main/java/com/iugu/model/Transfer.java
@@ -1,26 +1,38 @@
 package com.iugu.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.io.Serializable;
 
-public class Transfer {
+public class Transfer implements Serializable {
 
-	@JsonProperty("receiver_id")
-	private String receiverId;
+    @JsonProperty("receiver_id")
+    private String receiverId;
 
-	@JsonProperty("amount_cents")
-	private int amountCents;
+    @JsonProperty("amount_cents")
+    private int amountCents;
 
-	public Transfer(String receiverId, int amountCents) {
-		this.receiverId = receiverId;
-		this.amountCents = amountCents;
-	}
+    public Transfer() {
+    }
 
-	public String getReceiverId() {
-		return receiverId;
-	}
+    public Transfer(String receiverId, int amountCents) {
+        this.receiverId = receiverId;
+        this.amountCents = amountCents;
+    }
 
-	public int getAmountCents() {
-		return amountCents;
-	}
+    public String getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(String receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public int getAmountCents() {
+        return amountCents;
+    }
+
+    public void setAmountCents(int amountCents) {
+        this.amountCents = amountCents;
+    }
 
 }
