b520f9ca756de40cca951a658e0eb1d784420261
Rafael <rafael@tartigrado.com>
2018-03-29 09:27:30 -0300

    Mapeamento transferência de valores

---
+++ src/main/java/com/iugu/model/WebHook.java
@@ -0,0 +1,65 @@
+package com.iugu.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.io.Serializable;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class WebHook implements Serializable {
+
+    @JsonProperty(value = "event")
+    private String event;
+
+    @JsonProperty("data[id]")
+    private String id;
+
+    @JsonProperty("data[account_id]")
+    private String accountId;
+
+    @JsonProperty("data[status]")
+    private String status;
+
+    @JsonProperty("data[subscription_id]")
+    private String subscriptionId;
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getSubscriptionId() {
+        return subscriptionId;
+    }
+
+    public void setSubscriptionId(String subscriptionId) {
+        this.subscriptionId = subscriptionId;
+    }
+
+}
