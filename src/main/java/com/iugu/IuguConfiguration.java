81d44ee7c453e365922e14ff19828f55c1167379
Daniel Felgar <daniel@netpos.com.br>
2019-06-14 21:03:33 -0300

    Add proxy to iugu

--- src/main/java/com/iugu/IuguConfiguration.java
+++ src/main/java/com/iugu/IuguConfiguration.java
@@ -1,27 +1,43 @@
 package com.iugu;
 
 import com.iugu.serializers.GsonJsonProvider;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 
 public class IuguConfiguration {
 
     private final static String URL = "https://api.iugu.com/v1";
     private final String tokenId;
+    private String proxyHost;
+    private int proxyPort;
+
+    public IuguConfiguration(String token, String proxyHost, int proxyPort) {
+        tokenId = token;
+        this.proxyHost = proxyHost;
+        this.proxyPort = proxyPort;
+    }
 
     public IuguConfiguration(String token) {
         tokenId = token;
     }
 
-    public Client getNewClient() {
-        return ClientBuilder.newClient()
+    public ResteasyClient getNewClient() {
+        ResteasyClientBuilder builder = new ResteasyClientBuilder()
                 .register(new GsonJsonProvider())
                 .register(new Authenticator(tokenId, ""));
+        if (proxyHost != null && proxyPort > 0) {
+            builder.defaultProxy(proxyHost, proxyPort);
+        }
+        return builder.build();
     }
 
-    public Client getNewClientNotAuth() {
-        return ClientBuilder.newClient();
+    public ResteasyClient getNewClientNotAuth() {
+        ResteasyClientBuilder builder = new ResteasyClientBuilder()
+                .register(new GsonJsonProvider());
+        if (proxyHost != null && proxyPort > 0) {
+            builder.defaultProxy(proxyHost, proxyPort);
+        }
+        return builder.build();
     }
 
     public static String url(String endpoint) {
--- src/main/java/com/iugu/services/PaymentTokenService.java
+++ src/main/java/com/iugu/services/PaymentTokenService.java
@@ -11,28 +11,28 @@ import javax.ws.rs.core.Response;
 
 public class PaymentTokenService {
 
-	private IuguConfiguration iugu;
-	private final String CREATE_URL = IuguConfiguration.url("/payment_token");
+    private IuguConfiguration iugu;
+    private final String CREATE_URL = IuguConfiguration.url("/payment_token");
 
-	public PaymentTokenService(IuguConfiguration iuguConfiguration) {
-		this.iugu = iuguConfiguration;
-	}
+    public PaymentTokenService(IuguConfiguration iuguConfiguration) {
+        this.iugu = iuguConfiguration;
+    }
 
-	public PaymentTokenResponse create(PaymentToken paymentToken) throws IuguException {
-		Response response = this.iugu.getNewClientNotAuth().target(CREATE_URL).request().post(Entity.entity(paymentToken, MediaType.APPLICATION_JSON));
+    public PaymentTokenResponse create(PaymentToken paymentToken) throws IuguException {
+        Response response = this.iugu.getNewClientNotAuth().target(CREATE_URL).request().post(Entity.entity(paymentToken, MediaType.APPLICATION_JSON));
 
-		int ResponseStatus = response.getStatus();
-		String ResponseText = null;
+        int ResponseStatus = response.getStatus();
+        String ResponseText = null;
 
-		if (ResponseStatus == 200)
-			return response.readEntity(PaymentTokenResponse.class);
+        if (ResponseStatus == 200)
+            return response.readEntity(PaymentTokenResponse.class);
 
-		// Error Happened
-		if (response.hasEntity())
-			ResponseText = response.readEntity(String.class);
+        // Error Happened
+        if (response.hasEntity())
+            ResponseText = response.readEntity(String.class);
 
-		response.close();
+        response.close();
 
-		throw new IuguException("Error creating token!", ResponseStatus, ResponseText);
-	}
+        throw new IuguException("Error creating token!", ResponseStatus, ResponseText);
+    }
 }
