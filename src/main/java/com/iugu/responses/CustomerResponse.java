4be86c9a88557bdbe1af74fdb7f5267143f7e451
Daniel Felgar <daniel@netpos.com.br>
2019-06-27 12:08:26 -0300

    add defaultPaymentMethodId in customer response

--- src/main/java/com/iugu/responses/CustomerResponse.java
+++ src/main/java/com/iugu/responses/CustomerResponse.java
@@ -3,55 +3,65 @@ package com.iugu.responses;
 import java.util.List;
 
 public class CustomerResponse {
-	
-	private String id;
-	
-	private String email;
-	
-	private String name;
-	
-	private String notes;
-	
-	private List<CustomVariableResponse> customVariables;
 
-	public String getId() {
-		return id;
-	}
+    private String id;
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    private String email;
 
-	public String getEmail() {
-		return email;
-	}
+    private String name;
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    private String notes;
 
-	public String getName() {
-		return name;
-	}
+    private String defaultPaymentMethodId;
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    private List<CustomVariableResponse> customVariables;
 
-	public String getNotes() {
-		return notes;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public void setNotes(String notes) {
-		this.notes = notes;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public List<CustomVariableResponse> getCustomVariables() {
-		return customVariables;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setCustomVariables(List<CustomVariableResponse> customVariables) {
-		this.customVariables = customVariables;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public List<CustomVariableResponse> getCustomVariables() {
+        return customVariables;
+    }
+
+    public void setCustomVariables(List<CustomVariableResponse> customVariables) {
+        this.customVariables = customVariables;
+    }
+
+    public String getDefaultPaymentMethodId() {
+        return defaultPaymentMethodId;
+    }
+
+    public void setDefaultPaymentMethodId(String defaultPaymentMethodId) {
+        this.defaultPaymentMethodId = defaultPaymentMethodId;
+    }
 
 }
