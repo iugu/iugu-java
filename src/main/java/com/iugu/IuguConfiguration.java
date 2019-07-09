c61a499a1a695c08dc068864773bcb30fc46c28e
Rafael Ferreira da Costa <rafael@tartigrado.com>
2019-07-09 10:10:35 -0300

    Correção do erro de acesso a URL do IUGU através da API

--- src/main/java/com/iugu/IuguConfiguration.java
+++ src/main/java/com/iugu/IuguConfiguration.java
@@ -1,5 +1,9 @@
 package com.iugu;
 
+import java.security.cert.X509Certificate;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 
@@ -13,15 +17,46 @@ public class IuguConfiguration {
 	}
 
 	public Client getNewClient() {
-		return ClientBuilder.newClient().register(new Authenticator(tokenId, ""));
+		return ClientBuilder.newBuilder().sslContext(getContext()).build().register(new Authenticator(tokenId, ""));
 	}
 
 	public Client getNewClientNotAuth() {
-		return ClientBuilder.newClient();
+		return ClientBuilder.newBuilder().sslContext(getContext()).build();
 	}
 
 	public static String url(String endpoint) {
 		return URL + endpoint;
 	}
 
+        public SSLContext getContext() {
+            try {
+                TrustManager[] noopTrustManager = new TrustManager[]{
+                    new X509TrustManager() {
+
+                        @Override
+                        public X509Certificate[] getAcceptedIssuers() {
+                            return null;
+                        }
+
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+                        }
+                    }
+                };
+
+                SSLContext sc = SSLContext.getInstance("SSL");
+                sc.init(null, noopTrustManager, null);
+                
+                return sc;
+            } catch (Exception e) {
+                System.out.println(e.getCause());
+                return null;
+            }
+
+        }
+        
 }
